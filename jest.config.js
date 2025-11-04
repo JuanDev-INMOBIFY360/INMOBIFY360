5defe7019cbd37a2b516700ef0101d9f348881ce
JuanDev-INMOBIFY360 <Juan_Palomino@satrack.com>
2025-11-04 15:42:15 -0500

    ci

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-// jest.config.js
-export default {
-  testEnvironment: "node",
-  transform: {}, // evita errores con ESModules
-  setupFiles: ["dotenv/config"], // carga las variables del entorno
-  testMatch: ["**/tests/**/*.test.js"], // busca tus tests
-  verbose: true,
-};
