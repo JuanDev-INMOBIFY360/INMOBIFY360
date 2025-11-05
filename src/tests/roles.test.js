77b09f538353f51a508c2581aa5c3f1669dfca6e
JuanDev-INMOBIFY360 <Juan_Palomino@satrack.com>
2025-11-05 09:07:05 -0500

    probando datos

--- src/tests/roles.test.js
+++ src/tests/roles.test.js
@@ -16,5 +16,35 @@ describe("Pruebas de roles de usuario", () => {
     expect(newRole).toHaveProperty("id");
     expect(newRole.name).toBe("Tester");
   });
-});
+
+  it ("Debe obtener un rol de usuario por ID", async () => {
+    const role = await prisma.roles.findUnique({
+      where: { name: "Tester" },
+    });
+    expect(role).toHaveProperty("id");
+    expect(role.name).toBe("Tester");
+  });
+  it("Debe obtener todos los roles de usuario", async () =>{
+    const roles = await prisma.roles.findMany();
+    expect(Array.isArray(roles)).toBe(true);
+    expect(roles.length).toBeGreaterThan(0);
+  })
+
+  it("Debe actualizar un rol de usuario", async () =>{
+    const roleToUpdate = await prisma.roles.update({
+      where: { name: "Tester" },
+      data: { name: "Updated Tester" },
+    });
+    expect(roleToUpdate.name).toBe("Updated Tester");
+  });
+
+  it("Debe eliminar un rol de usuario", async () =>{
+    const roleToDelete = await prisma.roles.delete({
+      where: { name: "Updated Tester" },
+    });
+    expect(roleToDelete.name).toBe("Updated Tester");
+    })
+  });
+
+
 
